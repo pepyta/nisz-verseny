be7aedcba94a4b33b96caddd0ca2e4b978b7420c
Daerile <palldominiko8@gmail.com>
2021-04-10 17:50:39 +0200

    Update [id].tsx

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,23 +1,39 @@
 import FullScreenLoader from "@components/loaders/FullScreenLoader";
+import { Card, CardActionArea, CardContent, Container, Grid, Typography, Button } from "@material-ui/core";
 import { usePosts } from "@components/providers/PostsProvider";
 import Head from 'next/head'
 import { useRouter } from "next/router";
 
-export default function Post(){
+export default function Post() {
     const router = useRouter();
-    const id = parseInt(router.query.id+"");
+    const id = parseInt(router.query.id + "");
     const { posts, loaded } = usePosts();
 
     const post = posts.find((post) => post.id === id);
 
-    if(loaded && !post) router.push("/");
-    if(!post) return (<FullScreenLoader />); 
+    if (loaded && !post) router.push("/");
+    if (!post) return (<FullScreenLoader />);
 
     return (
-        <>
-            <section>
-                {post.title}
-            </section>
-        </>
+        <Container maxWidth="lg">
+            <Grid item xs={8}>
+                <Typography variant="h4">
+                    {post.title}
+
+                </Typography>
+            </Grid>
+            <Grid item xs={4}>
+                <Typography variant="body2">
+                    {`${post.author.image} ${post.author.name}`}
+                </Typography>
+            </Grid>
+
+            <Typography variant="body1">
+                {post.content}
+            </Typography>
+            <Button variant="contained" color="primary" href="../index.tsx">
+                Kezdőoldalra!
+            </Button>
+        </Container>
     )
 }
