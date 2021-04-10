a9548d111f1a0dcc7c4d059e192b2543d1761897
unknown <pepyta118@gmail.com>
2021-04-10 20:20:33 +0200

    new design for single post

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,6 +3,10 @@ import { Card, CardActionArea, CardContent, makeStyles, Container, Grid, Typogra
 import { usePosts } from "@components/providers/PostsProvider";
 import Head from 'next/head';
 import { useRouter } from "next/router";
+import MarkdownIt from "markdown-it";
+import Tag from "@components/tags/Tag";
+
+const md = new MarkdownIt();
 
 export default function Post() {
     const router = useRouter();
@@ -18,33 +22,55 @@ export default function Post() {
 
     return (
         <Container maxWidth="lg" >
-                <Card className={classes.cardContent}>
-                    <CardContent>
-                        <Grid item xs={12}>
-                            <Typography variant="h4" className={classes.header}>
-                                {post.title}                     
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={8}>
+                    <Card className={classes.cardContent}>
+                        <CardContent>
+                            <Typography variant="h5" component="h2" className={classes.header}>
+                                {post.title}
                             </Typography>
+                            <div dangerouslySetInnerHTML={{ __html: md.render(post.content + "") }} />
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <Card className={classes.cardContent}>
+                                <CardContent>
+                                    <Typography variant="h1" textAlign={"center"} gutterBottom>
+                                        {post.author.image}
+                                    </Typography>
+                                    <Typography variant="h6" textAlign={"center"}>
+                                        {post.author.name}
+                                    </Typography>
+                                    <Typography variant="caption" textAlign={"center"} component="div">
+                                        {post.author.email}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
                         </Grid>
-                    </CardContent>
-                </Card>
-            <Card className={classes.cardContent}>  
-                <CardContent>
-                    <Grid item xs={12}>
-                        <Typography variant="h5" component="h3" >
-                            {post.content}                     
-                        </Typography>
+                        <Grid item xs={12}>
+                            <Card>
+
+                                <CardContent>
+                                    <Typography variant="h5" gutterBottom>
+                                        Címkék
+                                    </Typography>
+                                    <Grid container spacing={1}>
+
+                                        {post.PostCategoryConnector.map(({ category }) => (
+                                            <Grid item>
+                                                <Tag category={category} />
+                                            </Grid>
+                                        ))}
+                                    </Grid>
+                                </CardContent>
+                            </Card>
+                        </Grid>
                     </Grid>
-                </CardContent>
-            </Card>
-            <Card className={classes.cardContent}>  
-                <CardContent>
-                    <Grid item xs={12}>
-                        <Typography variant="h5" component="h5" className={classes.footer}>
-                            {`${post.author.image} ${post.author.name}`}                      
-                        </Typography>
-                    </Grid>
-                </CardContent>
-            </Card>
+                </Grid>
+            </Grid>
         </Container>
     )
 }
@@ -52,13 +78,11 @@ export default function Post() {
 const useStyles = makeStyles(() => ({
     header: {
         textAlign: "left",
-        fontWeight: "fontWeightBold",
+        fontWeight: "bold",
     },
-
     footer: {
         textAlign: "left",
     },
-    
     cardContent: {
         marginTop: 3,
         marginBottom: 3,
