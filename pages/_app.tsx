f5e520dd0644adb31acadddcb9e34b4525e48c75
unknown <pepyta118@gmail.com>
2021-04-10 11:19:37 +0200

    Add snackbars

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,33 @@
 import { createMuiTheme, CssBaseline, ThemeProvider } from '@material-ui/core';
 import { AppProps } from 'next/dist/next-server/lib/router/router';
 import Head from "next/head";
+import { SnackbarProvider } from 'notistack';
 
 const theme = createMuiTheme({});
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
     return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Head>
-                <meta charSet="utf-8" />
-                <title>
-                    Verseny    
-                </title>
+        <SnackbarProvider>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Head>
+                    <meta charSet="utf-8" />
+                    <title>
+                        Verseny    
+                    </title>
 
-                <link
-                    rel="stylesheet"
-                    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-                />
-                <link
-                    rel="stylesheet"
-                    href="https://fonts.googleapis.com/icon?family=Material+Icons"
-                />
-            </Head>    
-            <Component {...pageProps} />
-        </ThemeProvider>
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                    />
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+                    />
+                </Head>    
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </SnackbarProvider>
     );
 };
 
