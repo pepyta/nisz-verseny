ce7aaccfca2c4bce86dcdfcce601dbf06734343b
Daerile <palldominiko8@gmail.com>
2021-04-10 11:47:52 +0200

    Create get.ts

---
+++ pages/api/posts/get.ts
@@ -0,0 +1,26 @@
+
+import wrapper from "../../../lib/server/endpoint";
+import prisma from "../../../lib/server/prisma";
+import getUser from "../../../lib/server/getUser";
+
+export default wrapper(async (req) => {
+    const user = await getUser(req);
+
+    const posts = await prisma.role.findMany({
+        where: {
+            userId: user.id,
+        },
+        include: {
+            author: true
+            PostCategoryConnector: {
+                include: {
+                    category: true
+                }
+            }
+        },
+    });
+
+    return {
+        data: posts,
+    };
+});
