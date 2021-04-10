d9607b886d6ddc4a1e28db7af05ee7b29766d424
unknown <pepyta118@gmail.com>
2021-04-10 10:49:03 +0200

    Password authentication

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,33 @@
+import prisma from "../../../lib/server/prisma";
+import NextAuth from "next-auth";
+import Adapters from "next-auth/adapters";
+import Providers from "next-auth/providers";
+import bcrypt from "bcryptjs";
+
+export default NextAuth({
+    providers: [
+        Providers.Credentials({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "email" },
+                password: { label: "Password", type: "password" },
+            },
+            authorize: async ({ email, password }) => {
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email,
+                    },
+                    include: {
+                        PasswordAuthentication: true,
+                    }
+                });
+
+                if(!user) return null;
+                if(!await bcrypt.compare(password, user.PasswordAuthentication.password)) return null;
+
+                return user;
+            },
+        }),
+    ],
+    adapter: Adapters.Prisma.Adapter({ prisma }),
+});
