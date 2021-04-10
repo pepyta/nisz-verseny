ca342707194a7f9f2553d50d981db0c2bcb283cc
unknown <pepyta118@gmail.com>
2021-04-10 11:24:09 +0200

    Create post endpoint

--- pages/api/posts/create.ts
+++ pages/api/posts/create.ts
@@ -2,19 +2,27 @@ import wrapper from "../../../lib/server/endpoint";
 import prisma from "../../../lib/server/prisma";
 import getUser from "../../../lib/server/getUser";
 
+export type CreatePostRequiredParameters = {
+    title: string;
+    content: string;
+    categories: string[];
+};
+
 export default wrapper(async (req) => {
     const user = await getUser(req);
 
-    const {title, content, post_category_connector} = JSON.parse(req.body);
+    const {title, content, categories}: CreatePostRequiredParameters = JSON.parse(req.body);
 
     const post = await prisma.post.create({
         data: {
             title,
             content,
-            post_category_connector: {
-                connect: {
-                    id: postId,
-                },
+            PostCategoryConnector: {
+                create: categories.map((category) => (
+                    {
+                        categoryId: category,
+                    }
+                )),
             },
             author: {
                 connect: {
